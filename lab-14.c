4d0cc2b8e91916372717f37045a4816cdef87929
U-DESKTOP-I2G3KNE\hotae <leeqkwkek@gmail.com>
2024-07-11 13:29:32 +0900

    lab-14.c

---
+++ lab-14.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int count = 0;
+
+    for (int A = 1; A <= 500; A++) {
+        for (int B = 1; B <= A; B++) {
+            if (A * A == B * B + N) {
+                count++;
+            }
+        }
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
+
